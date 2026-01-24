63872badfbc195194e2f8f9f25916489e214811e
Felippe Costa <felippemsc@gmail.com>
2026-01-24 20:55:09 -0300

    feat: add ExampleApp with SDK integration
    - SwiftUI app demonstrating SDK usage
    - Local package dependency on SignedShotSDK
    - Bundle ID: io.signedshot.sdk.ExampleApp

---
+++ ExampleApp/ExampleApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Felippe Costa on 24/01/26.
+//
+
+import SwiftUI
+import SignedShotSDK
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Felippe Costa on 24/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
